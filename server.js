a93ad79c35a9adfff45215fc14a626f34845a28f
shinamipc <nekonaitoway@yahoo.co.jp>
2021-08-01 17:56:41 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+  res.render('top.ejs');
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
